91630581bf820c16b6b163e37da39faf54f577f8
Shaleen, Adam (ETW - FLEX) <Adam.Shaleen@nike.com>
2019-03-27 12:35:20 -0700

    Where is this commit going?

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -26,14 +26,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userImage
+            guard let ciImage = CIImage(image: userImage) else {
+                fatalError("Error converting CI-Image")
+            }
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
 
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CorML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed processing image")
+            }
+            if let firstResult = results.first {
+                self.navigationItem.title = (firstResult.identifier.contains("hotdog")) ? "Hotdog" : "Not Hotdog"
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
-    
-    
 }
